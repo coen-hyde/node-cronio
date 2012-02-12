b7d03c4dadd7baa6cc44793dfaa653bcbae8b771
Coen Hyde <coen.hyde@gmail.com>
2012-02-13 08:07:44 +1000

    initial commit

---
+++ test/cronio.js
@@ -0,0 +1,22 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    util = require('util');
+
+vows.describe("Cron.io API").addBatch({
+  'Create user': {
+    topic: function () {
+
+    },
+    'returns a user': function () {
+
+    },
+    'Create a cron': {
+      topic: function () {
+
+      },
+      'returns a cron': {
+
+      }
+    }
+  }
+}).run();
